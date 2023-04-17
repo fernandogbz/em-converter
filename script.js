77194341f93dcb0858b353915bec7026de97279f
Fernando Jimenez <fgjv06@gmail.com>
2023-04-17 19:02:25 -0400

    JS - inpEM

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 // Getting variables for each input field
 let inpBase = document.getElementById("inp-base-px");
 let inpPX = document.getElementById("inp-px");
+let inpEM = document.getElementById("inp-em");
