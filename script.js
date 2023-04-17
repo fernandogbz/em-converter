3f2bfe923ddd3578acc978aad8694c69db96d81c
Fernando Jimenez <fgjv06@gmail.com>
2023-04-17 19:10:48 -0400

    JS - function to convert px to em

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ let inpBase = document.getElementById("inp-base-px");
 let inpPX = document.getElementById("inp-px");
 let inpEM = document.getElementById("inp-em");
 
+// Function to convert PX to EM
+let pxToEm = () => {
+  let inpBaseValue = inpBase.value;
+  let pxValue = inpPX.value;
+  console.log(inpBaseValue,pxValue)
+}
+
 // Adding oninput event to each input field
 inpPX.oninput = pxToEm;
